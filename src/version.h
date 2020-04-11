1ac09f5afd305ada4bb9bb7eb8d0f1e2c53bafa3
aleksdor <43136408+aleksdor@users.noreply.github.com>
2020-04-11 12:41:44 +0300

    Update version.h

--- src/version.h
+++ src/version.h
@@ -36,7 +36,7 @@ static const int PROTOCOL_VERSION = 62009;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 62005;
+static const int MIN_PEER_PROTO_VERSION = 62009;
 
 // minimum peer version accepted by DarkSendPool
 static const int MIN_POOL_PEER_PROTO_VERSION = 62005;
