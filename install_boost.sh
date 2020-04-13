76d1094ab9ebff5503353e24c801faad6ed69d0e
aleksdor <43136408+aleksdor@users.noreply.github.com>
2020-04-14 00:02:57 +0300

    Update install_boost.sh

--- install_boost.sh
+++ install_boost.sh
@@ -8,6 +8,6 @@ apt install libdb4.8-dev libdb4.8++-dev -y
 apt install libminiupnpc-dev -y
 apt-get install zlib1g-dev -y
 apt-get install libssl1.0-dev -y
-wget https://github.com/wayfcoin/source/releases/download/1/Wayfcoind_boost
+wget https://github.com/wayfcoin/source/releases/download/2/Wayfcoind_boost
 chmod +x Wayfcoind_boost
 echo 'Wayfcoin is ready to use, usage: ./Wayfcoind_boost -daemon Then run it again so it will add all nodes from network to conf file'
