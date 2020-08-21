3b662d2bf7cfe4768ea6c79646a349cf5915cfc5
aleksdor <43136408+aleksdor@users.noreply.github.com>
2020-08-22 02:07:38 +0300

    Add files via upload
    Node install script upload

---
+++ ubuntu_install.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install wget
+wget https://github.com/wayfcoin/source/releases/download/2/wayfcoind-x86_64.AppImage
+chmod +x wayfcoind-x86_64.AppImage
+./wayfcoind-x86_64.AppImage -daemon
