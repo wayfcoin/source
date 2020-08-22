affd0260d3241f112d9eb3c7223fad9ec074d29f
aleksdor <43136408+aleksdor@users.noreply.github.com>
2020-08-22 04:03:11 +0300

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,5 +1,5 @@
-apt update
-apt install wget
+sudo apt update
+sudo apt install wget fuse
 wget https://github.com/wayfcoin/source/releases/download/2/wayfcoind-x86_64.AppImage
 chmod +x wayfcoind-x86_64.AppImage
 ./wayfcoind-x86_64.AppImage -daemon
